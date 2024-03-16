a94fd15ef81bc4fd65131720d0de3022819d026f
3rick <erick.dejesus1@alumno.buap.mx>
2024-03-16 12:10:22 -0600

    add documentation plugin and other single types

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,20 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface UserComponent extends Schema.Component {
+  collectionName: 'components_user_components';
+  info: {
+    displayName: 'component';
+    icon: 'play';
+  };
+  attributes: {
+    a: Attribute.String;
+  };
+}
+
 declare module '@strapi/types' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'user.component': UserComponent;
+    }
+  }
 }
